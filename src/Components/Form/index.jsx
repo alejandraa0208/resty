56df72d859b1e99dcf579f533aa0c388c583deb9
ALEJANDRA ALTAMIRANO <a.altamirano0208@gmail.com>
2023-10-23 18:27:24 -0700

    completed lab26

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,37 +1,33 @@
 import React from 'react';
-
 import './Form.scss';
 
-class Form extends React.Component {
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const formData = {
-      method:'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
+function Form(props) {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = {
+            method: 'GET',
+            url: 'https://pokeapi.co/api/v2/pokemon',
+        };
+        props.handleApiCall(formData);
     };
-    this.props.handleApiCall(formData);
-  }
 
-  render() {
     return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <label >
-            <span>URL: </span>
-            <input name='url' type='text' />
-            <button type="submit">GO!</button>
-          </label>
-          <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
-          </label>
-        </form>
-      </>
+        <>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>URL: </span>
+                    <input name='url' type='text' />
+                    <button type="submit">GO!</button>
+                </label>
+                <label className="methods">
+                    <span id="get">GET</span>
+                    <span id="post">POST</span>
+                    <span id="put">PUT</span>
+                    <span id="delete">DELETE</span>
+                </label>
+            </form>
+        </>
     );
-  }
 }
 
 export default Form;
